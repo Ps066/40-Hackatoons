4c6795fc29f13653616df3fe35405ebc8b67194b
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2022-10-07 15:26:55 +0530

    second commit of frontpage

---
+++ templates/heropart2.php
@@ -0,0 +1,42 @@
+<!-- hero part 2 begin -->
+
+<div class="row text-center justify-content-evenly text-white bg-dark" style="font-family:'Times New Roman', Times, serif">
+          <div class="col-md-12 pt-3 pb-3" style="color:red;">
+            <hr style="color:rgb(136, 132, 132);">
+             <h1>Top Feature's</h1>
+          </div>
+        
+          <!-- part 1 start -->
+          <div class="col-md-4">
+            <img src="../assets/img/heropart2-image1.jpg" style="width:100%;">
+          </div>
+          <div class="col-md-6 pt-4">
+            <h3 style="color:red;">Email Tracking</h3>
+            <p class="pt-2" style="font-size:130%;">Email tracking involves tracking emails sent to people and using the results to your benefit. Most email tracking tools can tell you who opened your emails, the time they opened them, locations, and other additional details. Email tracking is a tested and true marketing tool, and it gives you a clearer image of your market and habits.</p>
+          </div>
+          <!-- part 1 end -->
+          <!-- part 2 begin -->
+          <div class="row mt-4 justify-content-evenly">
+          <div class="col-md-4 img-hero2">
+          <img src="../assets/img/heropart2-image2.jpg" style="width:100%;">
+            
+          </div>
+          <div class="col-md-6 pt-4">
+            <h3 style="color:red;">Email Analysis</h3>
+            <p class="pt-2" style="font-size:130%;">Email analytics is the method of tracking how your subscribers interact with your emails by tracking different metrics that deal with email delivery, engagement, and conversions.Such analytics helps email marketers to draw valuable insights and optimize and strategize email campaigns for better performance.</p>
+          </div>
+        </div>
+          <!-- part 2 end -->
+          <!-- part 3 start -->
+          <div class="row mt-4 justify-content-evenly">
+          <div class="col-md-4 img-hero2">
+          <img src="../assets/img/heropart2-image3.png" style="width:100%;">
+          </div>
+          <div class="col-md-6 pt-4">
+            <h3 style="color:red;">Email Scheduling</h3>
+            <p class="pt-2" style="font-size:130%;">Email scheduling is the process of setting your emails to automatically go out at a time of your choosing. That way, you can schedule your emails to go out in bulk at a pre-determined time to reach your audience .Sometime we even schedule our emails in different batches for audiences located in different time zones, which will allow you to reach them at optimal times.</p>
+          </div>
+        </div>
+        </div>
+          <!-- part 3 end -->
+          <!-- hero part 2 end -->
