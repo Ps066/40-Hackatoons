e19980a45806c897012ffe6ad87627518b71014a
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2022-10-08 13:38:48 +0530

    might be last commit

--- admin/admin_site/templates/navside_start.php
+++ admin/admin_site/templates/navside_start.php
@@ -81,7 +81,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="charts.html">
                     <i class="fas fa-fw fa-user"></i>
-                    <span>User</span></a>
+                    <span>Prashant</span></a>
                 </li>
             
             <!-- advisor  -->
@@ -99,7 +99,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="tables.html">
                         <i class="fas fa-fw fa-table"></i>
-                        <span>Tables</span></a>
+                        <span>Issues</span></a>
                     </li>
                     
                     <li class="nav-item">
